db1f2a2fcaab8026c55540b6cc59594d12320f12
Parasamitbahadur <Amitbahadur@parastechnologies.com>
2023-10-18 04:01:19 +0530

    validation in add card done

--- lib/core/presentation/widgets/add_new_card_popup.dart
+++ lib/core/presentation/widgets/add_new_card_popup.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:GetsbyRideshare/core/presentation/widgets/custom_button/custom_button_widget.dart';
 import 'package:GetsbyRideshare/core/presentation/widgets/custom_text_field.dart';
 import 'package:GetsbyRideshare/core/static/colors.dart';
@@ -24,184 +26,216 @@ class AddNewCardPopUp extends StatelessWidget {
         // height: 300,
         child: Form(
           key: provider.formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SvgPicture.asset(
-                  'assets/icons/payment_line.svg',
-                  color: grey707070Color.withOpacity(.5),
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-
-              //Enter card number
-              CustomTextField(
-                fillColor: greyE7E7E7Color.withOpacity(.2),
-                hintStyle: const TextStyle(
-                    fontFamily: 'poPPinRegular',
-                    fontWeight: FontWeight.w400,
-                    color: grey9c9c9cColor,
-                    fontSize: 12.0),
-                placeholder: "Enter Card Number",
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SvgPicture.asset('assets/icons/card.svg'),
-                ),
-                controller:
-                    Provider.of<PaymentProvider>(context).cardNumberController,
-                fieldValidator: (val) {
-                  if (val == '') {
-                    return appLoc.mustNotEmpty;
-                  }
-                  return null;
-                },
-              ),
-
-// Enter CVV Number
-              CustomTextField(
-                fillColor: greyE7E7E7Color.withOpacity(.2),
-                hintStyle: const TextStyle(
-                    fontFamily: 'poPPinRegular',
-                    fontWeight: FontWeight.w400,
-                    color: grey9c9c9cColor,
-                    fontSize: 12.0),
-                placeholder: "Enter CVV Number",
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SvgPicture.asset('assets/icons/card.svg'),
-                ),
-                controller:
-                    Provider.of<PaymentProvider>(context).cardCvvController,
-                fieldValidator: (val) {
-                  if (val == '') {
-                    return appLoc.mustNotEmpty;
-                  }
-                  return null;
-                },
-              ),
-
-              //Account Holder name
-              CustomTextField(
-                fillColor: greyE7E7E7Color.withOpacity(.2),
-                hintStyle: const TextStyle(
-                    fontFamily: 'poPPinRegular',
-                    fontWeight: FontWeight.w400,
-                    color: grey9c9c9cColor,
-                    fontSize: 12.0),
-                placeholder: "Account Holder Name",
-                prefixIcon: Padding(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: SvgPicture.asset(
-                    'assets/icons/user.svg',
+                    'assets/icons/payment_line.svg',
+                    color: grey707070Color.withOpacity(.5),
                   ),
                 ),
-                controller: Provider.of<PaymentProvider>(context)
-                    .accountHolderController,
-                fieldValidator: (val) {
-                  if (val == '') {
-                    return appLoc.mustNotEmpty;
-                  }
-                  return null;
-                },
-              ),
-
-              //Expiry Date
-              CustomTextField(
-                fillColor: greyE7E7E7Color.withOpacity(.2),
-                hintStyle: const TextStyle(
-                    fontFamily: 'poPPinRegular',
-                    fontWeight: FontWeight.w400,
-                    color: grey9c9c9cColor,
-                    fontSize: 12.0),
-                placeholder: "Expiry Date (yyyy/mm)",
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SvgPicture.asset(
-                    'assets/icons/calender.svg',
-                    width: 20.0,
-                    height: 20.0,
-                    fit: BoxFit.fill,
-                  ),
+                const SizedBox(
+                  height: 40,
                 ),
-                controller:
-                    Provider.of<PaymentProvider>(context).expiryController,
-                fieldValidator: (val) {
-                  if (val == '') {
-                    return appLoc.mustNotEmpty;
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * .05,
-              ),
 
-              //Save Button
-              CustomButton(
-                text: const Text(
-                  "Save",
-                  style: TextStyle(
-                    fontFamily: 'poPPinSemiBold',
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
+                //Enter card number
+                CustomTextField(
+                  fillColor: greyE7E7E7Color.withOpacity(.2),
+                  hintStyle: const TextStyle(
+                      fontFamily: 'poPPinRegular',
+                      fontWeight: FontWeight.w400,
+                      color: grey9c9c9cColor,
+                      fontSize: 12.0),
+                  placeholder: "Enter Card Number",
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SvgPicture.asset('assets/icons/card.svg'),
                   ),
-                ),
-                buttonHeight: 50,
-                isRounded: true,
-                event: () {
-                  final provider = context.read<PaymentProvider>();
-                  provider.addCardDetails().listen((state) async {
-                    switch (state.runtimeType) {
-                      case AddCardLoading:
-                        showLoading();
-                        break;
-                      case AddCardFailure:
-                        final msg = (state as AddCardFailure).failure;
-                        dismissLoading();
-                        showToast(message: msg);
-                        break;
-                      case AddCardSuccess:
-                        final data = (state as AddCardSuccess).data;
-                        dismissLoading();
-                        if (data.success == 1) {
-                          showToast(message: "add card success");
-                          Navigator.pop(context);
-                          // Navigator.pushNamedAndRemoveUntil(
-                          //     context, HomePage.routeName, (route) => false);
-                        } else {
-                          if (data.message == '1') {
-                            showToast(message: appLoc.registrationFailed);
-                          } else {
-                            showToast(message: appLoc.registrationFailed);
-                          }
-                        }
-
-                        break;
+                  controller: Provider.of<PaymentProvider>(context)
+                      .cardNumberController,
+                  fieldValidator: (val) {
+                    if (val == '') {
+                      return appLoc.mustNotEmpty;
                     }
-                  });
+                    return null;
+                  },
+                ),
 
-                  // Stripe.instance.createToken(
-                  //   CreateTokenParams.card(
-                  //     params: CardTokenParams(
-                  //         type: TokenType.Card,
-                  //         address: Address(
-                  //             city: "mohali",
-                  //             country: "India",
-                  //             line1: "line1",
-                  //             line2: "line2",
-                  //             postalCode: "140603",
-                  //             state: "Mohali"),
-                  //         currency: 'IN'),
-                  //   ),
-                  // );
-                },
-                bgColor: black080809Color,
-              ),
-            ],
+                // Enter CVV Number
+                CustomTextField(
+                  fillColor: greyE7E7E7Color.withOpacity(.2),
+                  hintStyle: const TextStyle(
+                      fontFamily: 'poPPinRegular',
+                      fontWeight: FontWeight.w400,
+                      color: grey9c9c9cColor,
+                      fontSize: 12.0),
+                  placeholder: "Enter CVV Number",
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SvgPicture.asset('assets/icons/card.svg'),
+                  ),
+                  controller:
+                      Provider.of<PaymentProvider>(context).cardCvvController,
+                  fieldValidator: (val) {
+                    if (val == '') {
+                      return appLoc.mustNotEmpty;
+                    }
+                    return null;
+                  },
+                ),
+
+                //Account Holder name
+                CustomTextField(
+                  fillColor: greyE7E7E7Color.withOpacity(.2),
+                  hintStyle: const TextStyle(
+                      fontFamily: 'poPPinRegular',
+                      fontWeight: FontWeight.w400,
+                      color: grey9c9c9cColor,
+                      fontSize: 12.0),
+                  placeholder: "Account Holder Name",
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SvgPicture.asset(
+                      'assets/icons/user.svg',
+                    ),
+                  ),
+                  controller: Provider.of<PaymentProvider>(context)
+                      .accountHolderController,
+                  fieldValidator: (val) {
+                    if (val == '') {
+                      return appLoc.mustNotEmpty;
+                    }
+                    return null;
+                  },
+                ),
+
+                //Expiry Date
+                GestureDetector(
+                  onTap: () {
+                    // log("celcil sdfa");
+                    // var date = DatePickerDialog(
+                    //     initialDate: DateTime.now(),
+                    //     firstDate: DateTime.now(),
+                    //     lastDate: DateTime(DateTime.now().year + 20));
+                  },
+                  child: CustomTextField(
+                    onTap: () {
+                      log("celcil sdfa");
+                      var date = DatePickerDialog(
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(DateTime.now().year + 20));
+                    },
+                    readOnly: true,
+                    fillColor: greyE7E7E7Color.withOpacity(.2),
+                    hintStyle: const TextStyle(
+                        fontFamily: 'poPPinRegular',
+                        fontWeight: FontWeight.w400,
+                        color: grey9c9c9cColor,
+                        fontSize: 12.0),
+                    placeholder: "Expiry Date (yyyy/mm)",
+                    prefixIcon: GestureDetector(
+                      onTap: () {
+                        log("asdf");
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: SvgPicture.asset(
+                          'assets/icons/calender.svg',
+                          width: 20.0,
+                          height: 20.0,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    controller:
+                        Provider.of<PaymentProvider>(context).expiryController,
+                    fieldValidator: (val) {
+                      if (val == '') {
+                        return appLoc.mustNotEmpty;
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .05,
+                ),
+
+                //Save Button
+                CustomButton(
+                  text: const Text(
+                    "Save",
+                    style: TextStyle(
+                      fontFamily: 'poPPinSemiBold',
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  buttonHeight: 50,
+                  isRounded: true,
+                  event: () {
+                    final provider = context.read<PaymentProvider>();
+
+                    if (provider.cardNumberController.text.trim() == '' ||
+                        provider.cardCvvController.text.trim() == '' ||
+                        provider.accountHolderController.text.trim() == '' ||
+                        provider.expiryController.text.trim() == '') {
+                      showToast(message: "Please fill all the fields");
+                    } else {
+                      final provider = context.read<PaymentProvider>();
+                      provider.addCardDetails().listen((state) async {
+                        switch (state.runtimeType) {
+                          case AddCardLoading:
+                            showLoading();
+                            break;
+                          case AddCardFailure:
+                            final msg = (state as AddCardFailure).failure;
+                            dismissLoading();
+                            showToast(message: msg);
+                            break;
+                          case AddCardSuccess:
+                            final data = (state as AddCardSuccess).data;
+                            dismissLoading();
+                            if (data.success == 1) {
+                              showToast(message: "add card success");
+                              Navigator.pop(context);
+                              // Navigator.pushNamedAndRemoveUntil(
+                              //     context, HomePage.routeName, (route) => false);
+                            } else {
+                              if (data.message == '1') {
+                                showToast(message: appLoc.registrationFailed);
+                              } else {
+                                showToast(message: appLoc.registrationFailed);
+                              }
+                            }
+
+                            break;
+                        }
+                      });
+                    }
+                    // Stripe.instance.createToken(
+                    //   CreateTokenParams.card(
+                    //     params: CardTokenParams(
+                    //         type: TokenType.Card,
+                    //         address: Address(
+                    //             city: "mohali",
+                    //             country: "India",
+                    //             line1: "line1",
+                    //             line2: "line2",
+                    //             postalCode: "140603",
+                    //             state: "Mohali"),
+                    //         currency: 'IN'),
+                    //   ),
+                    // );
+                  },
+                  bgColor: black080809Color,
+                ),
+              ],
+            ),
           ),
         ),
       );
