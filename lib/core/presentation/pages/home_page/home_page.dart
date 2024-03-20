bc3d4e06a20fd6749f84f953f75bb5f102cf1c09
Ankit Bisht <ankitbisht@parastechnologies.com>
2024-03-20 15:06:22 +0530

    height increases

--- lib/core/presentation/pages/home_page/home_page.dart
+++ lib/core/presentation/pages/home_page/home_page.dart
@@ -10,6 +10,7 @@ import 'package:GetsbyRideshare/core/utility/helper.dart';
 import 'package:GetsbyRideshare/core/utility/injection.dart';
 import 'package:GetsbyRideshare/core/utility/session_helper.dart';
 import 'package:GetsbyRideshare/socket/deryde_folder/chat/provider/test_socket_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -308,8 +309,12 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Container(
-                              padding: EdgeInsets.only(top: 15,bottom: 15),
-                              color: whiteColor,
+                              padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                              margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                              decoration: BoxDecoration(
+                                  color: whiteColor,
+                                borderRadius: BorderRadius.circular(10)
+                              ),
                               child: map.originAddress == ''
                                   ? Center(
                                       child: Padding(
@@ -344,27 +349,29 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                                               ),
                                             ],
                                           ),
-                                          Column(
-                                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                            children: [
-                                              OriginWidget(
-                                                deviceWidth: _deviceSize.width,
-                                                isFromOrder: false,
-                                              ),
-                                              Container(
-                                                margin: EdgeInsets.zero,
-                                                width: _deviceSize.width * .8,
-                                                height: 1.0,
-                                                color: whiteEFEFEFColor,
-                                              ),
-                                              Container(
-                                                child: DestinationWidget(
-                                                  deviceWidth:
-                                                      _deviceSize.width,
+                                          Expanded(
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                              children: [
+                                                OriginWidget(
+                                                  deviceWidth: _deviceSize.width,
                                                   isFromOrder: false,
                                                 ),
-                                              ),
-                                            ],
+                                                Container(
+                                                  margin: EdgeInsets.zero,
+                                                  width: _deviceSize.width * .8,
+                                                  height: 1.0,
+                                                  color: whiteEFEFEFColor,
+                                                ),
+                                                Container(
+                                                  child: DestinationWidget(
+                                                    deviceWidth:
+                                                        _deviceSize.width,
+                                                    isFromOrder: false,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
                                           )
                                         ],
                                       )),
